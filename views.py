2e681fe501dcd5a7387609280a4414431e3a0fa2
Jonathan Perry <Jonathan Perry>
2016-10-08 12:58:40 -0400

    First commit of files to github project

---
+++ views.py
@@ -0,0 +1,18 @@
+from flask import render_template, request
+from app import app
+from schedule_api import get_terms
+
+@app.route('/')
+def index():
+    options = {}
+    try:
+        options['terms'] = get_terms()
+    except:
+        options['api_error'] = True
+
+    return render_template('index.html', **options)
+
+@app.route('/projects/')
+def projects():
+	return render_template('projects.html')
+	
